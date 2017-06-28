35f9935dbed932486eab4e44d2a0494eae70e428
Shogun <shogun@cowtech.it>
2017-06-28 14:42:27 +0200

    Added missing files.

---
+++ ember.js
@@ -0,0 +1,18 @@
+/*
+ * This file is part of the @cowtech/eslint-config npm package. Copyright (C) 2017 and above Shogun <shogun@cowtech.it>.
+ * Licensed under the MIT license, which can be found at http://www.opensource.org/licenses/mit-license.php.
+ */
+
+module.exports = {
+  extends: "./node",
+  env: {
+    es6: true,
+    node: false,
+    jquery: true,
+    browser: true
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module"
+  }
+};
