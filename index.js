06adc080d8cc9918a4528d81b7f3fa22c1c35327
Shogun <shogun@cowtech.it>
2018-05-09 10:41:12 +0200

    fix: Make compatible with prettier.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 module.exports = {
   extends: ['standard'],
-  rules: {}
+  rules: {
+    /*
+      This is inserted to make this compatible with prettier.
+      Once https://github.com/prettier/prettier/issues/3845 and https://github.com/prettier/prettier/issues/3847 are solved this might be not needed any more.
+    */
+    'space-before-function-paren': ['error', 'never']
+  }
 }
