228e9e06d15efac9bae3fec9ece348d897c52f7e
Shogun <shogun@cowtech.it>
2019-02-24 12:24:53 +0100

    feat: Updated to ESLint 5.x.x.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
       This is inserted to make this compatible with prettier.
       Once https://github.com/prettier/prettier/issues/3845 and https://github.com/prettier/prettier/issues/3847 are solved this might be not needed any more.
     */
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    curly: [2, 'all']
   }
 }
