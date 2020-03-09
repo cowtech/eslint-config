282a533e260b14a7e0e9d5f8eb330e13c063108b
Shogun <shogun@cowtech.it>
2020-03-09 13:25:23 +0100

    fix: Fixed wrong rules.

--- typescript.js
+++ typescript.js
@@ -2,14 +2,14 @@ module.exports = {
   extends: ['standard-with-typescript'],
   rules: {
     curly: [2, 'all'],
-    '@typescript-eslint/array-type': [2, { default: 'array', readonly: 'array' }],
+    '@typescript-eslint/array-type': [2, { default: 'generic', readonly: 'generic' }],
     '@typescript-eslint/no-non-null-assertion': 0,
     /*
       This is inserted to make this compatible with prettier.
       Once https://github.com/prettier/prettier/issues/3845 and https://github.com/prettier/prettier/issues/3847 are solved this might be not needed any more.
     */
     '@typescript-eslint/space-before-function-paren': 0,
-    '@typescript-eslint/strict-boolean-expressions': [2, { allowNullable: true }],
+    '@typescript-eslint/strict-boolean-expressions': [2, { allowNullable: true, allowSafe: true }],
     '@typescript-eslint/typedef': [
       2,
       {
