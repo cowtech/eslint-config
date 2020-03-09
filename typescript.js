cb7e8c1bc55d24ef060b65c705b1295403714125
Shogun <shogun@cowtech.it>
2020-03-09 13:29:27 +0100

    fix: Added missing rule.

--- typescript.js
+++ typescript.js
@@ -4,6 +4,9 @@ module.exports = {
     curly: [2, 'all'],
     '@typescript-eslint/array-type': [2, { default: 'generic', readonly: 'generic' }],
     '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/promise-function-async': 0,
+    '@typescript-eslint/require-await': 0
+    '@typescript-eslint/restrict-template-expressions': 0,
     /*
       This is inserted to make this compatible with prettier.
       Once https://github.com/prettier/prettier/issues/3845 and https://github.com/prettier/prettier/issues/3847 are solved this might be not needed any more.
