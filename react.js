f4d5d6e564909c0594f652e4aec07041b78a682a
Paolo Insogna <paolo@cowtech.it>
2020-05-10 09:17:24 +0200

    fix: Fixed React rules, again.

--- react.js
+++ react.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   rules: {
     'jsx-quotes': [2, 'prefer-double'],
-    'react/no-string-ref': 2,
+    'react/no-string-refs': 2,
     'react/prop-types': 0,
     'react/jsx-boolean-value': [2, 'always']
   }
