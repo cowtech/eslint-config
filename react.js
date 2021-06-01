8c5694cd83e3a6b41b1dccdfd3e187dbb3846064
Paolo Insogna <paolo@cowtech.it>
2021-06-01 14:45:08 +0200

    fix: Sorted lines.

--- react.js
+++ react.js
@@ -5,9 +5,9 @@ module.exports = {
   },
   rules: {
     'jsx-quotes': [2, 'prefer-double'],
+    'react/jsx-boolean-value': [2, 'always']
     'react/no-string-refs': 2,
     'react/prop-types': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-boolean-value': [2, 'always']
+    'react/react-in-jsx-scope': 0
   }
 }
--- typescript.js
+++ typescript.js
@@ -6,8 +6,8 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 0,
     '@typescript-eslint/promise-function-async': 0,
     '@typescript-eslint/require-await': 0,
-    '@typescript-eslint/return-await': 0,
     '@typescript-eslint/restrict-template-expressions': 0,
+    '@typescript-eslint/return-await': 0,
     /*
       This is inserted to make this compatible with prettier.
       Once https://github.com/prettier/prettier/issues/3845 and https://github.com/prettier/prettier/issues/3847 are solved this might be not needed any more.
