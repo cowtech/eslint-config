33420b5e57508cbc2e9a16878c58a56ea370527e
Paolo Insogna <paolo@cowtech.it>
2021-06-01 14:43:46 +0200

    fix: Do not require React for JSX.

--- react.js
+++ react.js
@@ -7,6 +7,7 @@ module.exports = {
     'jsx-quotes': [2, 'prefer-double'],
     'react/no-string-refs': 2,
     'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
     'react/jsx-boolean-value': [2, 'always']
   }
 }
