5bd98c96acb087cf889e37fc306ae5fcf47aa5bf
Shogun <ShogunPanda@users.noreply.github.com>
2022-01-14 15:54:11 +0100

    feat: Allow functions in JSX.

--- react-with-typescript.js
+++ react-with-typescript.js
@@ -6,6 +6,7 @@ module.exports = {
   rules: {
     'jsx-quotes': [2, 'prefer-double'],
     'react/jsx-boolean-value': [2, 'always'],
+    'react/jsx-no-bind': [2, { allowFunctions: true }],
     'react/no-string-refs': 2,
     'react/prop-types': 0,
     'react/react-in-jsx-scope': 0
