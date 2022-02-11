3a559ef433f8f7f6ddf534eef24dfd3659951ace
Shogun <ShogunPanda@users.noreply.github.com>
2022-02-11 06:39:00 +0100

    feat: Added import rule.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: ['standard'],
   rules: {
     curly: [2, 'all'],
-    'no-return-await': 2,
+    'import/extensions': [2, 'always', { ignorePackages: true }], // This is required for proper ESM use
+    'no-return-await': 2, // Do not create useless promises
     'space-before-function-paren': 0 // This is inserted to make this compatible with prettier.
   },
   reportUnusedDisableDirectives: true
